eded738aaa5164b7c57bd0391f61000cdc019e98
Kiarash <kiamhn1998@gmail.com>
2020-12-08 21:54:53 +0330

    first commit

---
+++ src/main/java/com/document/documentTranslator/DocumentTranslatorApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.document.documentTranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentTranslatorApplication.class, args);
+	}
+
+}
