2c098c8474bc30d626e05462ed89f6f6a60d2715
Kiarash <kiamhn1998@gmail.com>
2020-12-08 22:54:58 +0330

    Add User Register & Login

---
+++ src/main/java/com/document/documentTranslator/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.document.documentTranslator.controller;
+
+import com.document.documentTranslator.dto.UserDto;
+import com.document.documentTranslator.enums.ResponseMessages;
+import com.document.documentTranslator.exception.DomainException;
+import com.document.documentTranslator.response.Response;
+import com.document.documentTranslator.service.User.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody UserDto dto) throws DomainException {
+
+        return ResponseEntity.ok().body(new Response(ResponseMessages.SUCCESSFUL, userService.createUser(dto),
+                true, null));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@RequestBody UserDto dto) throws DomainException {
+
+        return ResponseEntity.ok().body(new Response(ResponseMessages.SUCCESSFUL, userService.login(dto),
+                true, null));
+    }
+}
