904d0c7786bdf75f2c295914ad22e16d6286a184
Kiarash <kiamhn1998@gmail.com>
2021-01-29 00:10:35 +0330

    - Edit cors config

--- src/main/java/com/document/documentTranslator/configuration/CORSConfiguration.java
+++ src/main/java/com/document/documentTranslator/configuration/CORSConfiguration.java
@@ -15,7 +15,7 @@ public class CORSConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH").allowedOriginPatterns("*");
             }
         };
     }
