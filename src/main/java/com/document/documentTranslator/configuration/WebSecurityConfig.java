f9705e70b00b07793a87483d73eeed9dab5318c0
Kiarash <kiamhn1998@gmail.com>
2021-01-30 16:56:32 +0330

    - Edit cors config

---
+++ src/main/java/com/document/documentTranslator/configuration/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.document.documentTranslator.configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+    }
+}
